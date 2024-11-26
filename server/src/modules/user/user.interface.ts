1b18046a9c1ba043e79a065d9ea7cf981f67eda3
ashiqurras <ashiqurrussel2@gmail.com>
2024-11-26 20:26:13 +0100

    User type and schema created

--- server/src/modules/user/user.interface.ts
+++ server/src/modules/user/user.interface.ts
@@ -1,1 +1,8 @@
-// user interface
+export type TUser = {
+  id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'admin' | 'student' | 'faculty';
+  status: 'in-progress' | 'blocked';
+  isDeleted: boolean;
+};
