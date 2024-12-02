477ee1ca1082d5579885fc8137e2ecd0a7dbdd6e
ashiqurras <ashiqurrussel2@gmail.com>
2024-12-02 17:55:06 +0100

    validation with zod when student create

--- server/src/app/modules/user/user.route.ts
+++ server/src/app/modules/user/user.route.ts
@@ -1,8 +1,30 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { UserController } from './user.controller';
+import { AnyZodObject } from 'zod';
+import { createStudentValidationSchema } from '../student/student.validation';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent);
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // validation check
+      //if everything allright next() ->
+      await schema.parseAsync({
+        body: req.body,
+      });
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+router.post(
+  '/create-student',
+  validateRequest(createStudentValidationSchema),
+  UserController.createStudent,
+);
 
 export const UserRoutes = router;
