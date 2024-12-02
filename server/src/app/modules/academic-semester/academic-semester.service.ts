2c87ccd3349028b231d15cec5763be29f21243fe
ashiqurras <ashiqurrussel2@gmail.com>
2024-12-03 00:46:18 +0100

    More validation for semster code and name check for prevent duplicate semester creation

--- server/src/app/modules/academic-semester/academic-semester.service.ts
+++ server/src/app/modules/academic-semester/academic-semester.service.ts
@@ -1,7 +1,11 @@
-import { TAcademicSemesterCode } from './academic-semester.interface';
+import { academicSemesterNameCodeMapper } from './academic-semester.constant';
+import { TAcademicSemester } from './academic-semester.interface';
 import { AcademicSemester } from './academic-semester.model';
 
-const createAcademicSemester = async (payload: TAcademicSemesterCode) => {
+const createAcademicSemester = async (payload: TAcademicSemester) => {
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code !');
+  }
   return await AcademicSemester.create(payload);
 };
 
