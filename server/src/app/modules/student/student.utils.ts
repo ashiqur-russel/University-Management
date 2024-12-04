272e8c286051d84820e0663c5eb6f67dad23d0d3
ashiqurras <ashiqurrussel2@gmail.com>
2024-12-04 16:12:05 +0100

    Fix bug for creating student id for different semester

--- server/src/app/modules/student/student.utils.ts
+++ server/src/app/modules/student/student.utils.ts
@@ -4,20 +4,43 @@ import { TStudent } from './student.interface';
 
 export const generateStudentId = async (studentData: TStudent) => {
   try {
+    let studentId;
+    let incrementId;
     const result = await AcademicSemester.findById(
       studentData.admissionSemester,
     );
+
     if (!result || !result.code || !result.year) {
       throw new Error('Invalid result or missing code/year.');
     }
-    const currentId =
-      (await findLastStudentId()) || (0).toString().padStart(4, '0');
-    let incrementId = (Number(currentId) + 1).toString();
 
-    incrementId = incrementId.toString().padStart(4, '0');
-    const studentId = `${result?.year}-${result?.code}-${incrementId}`;
+    const lastStudentId = await findLastStudentId();
 
-    return studentId.toString();
+    const lastRegisteredIdYear = lastStudentId?.substring(0, 4);
+    const lastRegisteredIdCode = lastStudentId?.substring(5, 7);
+    const lastRegisteredId = lastStudentId?.substring(8);
+
+    if (
+      lastStudentId &&
+      lastRegisteredId !== undefined &&
+      lastRegisteredIdYear === result.year &&
+      lastRegisteredIdCode === result.code
+    ) {
+      studentId = Number(lastRegisteredId) + 1;
+
+      incrementId = studentId.toString().padStart(4, '0');
+      studentId = `${result?.year}-${result?.code}-${incrementId}`;
+
+      return studentId;
+    } else {
+      const currentId = (0).toString().padStart(4, '0');
+      incrementId = (Number(currentId) + 1).toString();
+
+      incrementId = incrementId.toString().padStart(4, '0');
+      studentId = `${result?.year}-${result?.code}-${incrementId}`;
+
+      return studentId;
+    }
   } catch (error) {
     console.error(error);
     throw new Error('Error generating student id');
@@ -35,7 +58,7 @@ export const findLastStudentId = async () => {
       .sort({ createdAt: -1 })
       .lean();
 
-    return lastStudent?.id ? lastStudent.id.substring(8) : undefined;
+    return lastStudent?.id ? lastStudent.id : undefined;
   } catch (error) {
     console.error(error);
     throw new Error('Error fining student id');
