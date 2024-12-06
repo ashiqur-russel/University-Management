3387f01879f42d8438ad7af743ddd3cdcc1ddea1
ashiqurras <ashiqurrussel2@gmail.com>
2024-12-06 23:16:01 +0100

    Fix duplicate name issue while creating department by pre hook validation

--- server/src/app/modules/academic-department/academic-department.model.ts
+++ server/src/app/modules/academic-department/academic-department.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { model, Schema } from 'mongoose';
 import { TAcademicDepartment } from './academic-department.interface';
+import { AcademicFaculty } from '../academic-faculty/academic-faculty.model';
 
 const AcademicDepartemntSchema = new Schema<TAcademicDepartment>(
   {
@@ -19,6 +20,24 @@ const AcademicDepartemntSchema = new Schema<TAcademicDepartment>(
     timestamps: true,
   },
 );
+
+AcademicDepartemntSchema.pre('save', async function (next) {
+  const facultyExist = await AcademicFaculty.findOne({ _id: this.faculty });
+  const departmentAlreadyExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (!facultyExist) {
+    const error = new Error('Faculty does not exist!');
+    return next(error);
+  }
+  if (departmentAlreadyExist) {
+    const error = new Error('Department already exist!');
+    return next(error);
+  }
+
+  next();
+});
 
 export const AcademicDepartment = model(
   'AcademicDepartemnt',
