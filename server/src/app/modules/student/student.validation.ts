f9a9e11464b0c0cc1085579ed4c698209dc51d5d
ashiqur-russel <ashiqurrussel2@gmail.com>
2024-12-13 18:40:19 +0100

    Fix error making optional field while update

--- server/src/app/modules/student/student.validation.ts
+++ server/src/app/modules/student/student.validation.ts
@@ -55,55 +55,46 @@ export const createStudentValidationSchema = z.object({
 });
 
 const updateUserNameValidationSchema = z.object({
-  firstName: z
-    .string()
-    .min(1)
-    .max(20)
-    .refine((value) => /^[A-Z]/.test(value), {
-      message: 'First Name must start with a capital letter',
-    }),
-  middleName: z.string(),
-  lastName: z.string(),
+  firstName: z.string().min(1).max(20).optional(),
+  middleName: z.string().optional(),
+  lastName: z.string().optional(),
 });
 
 const updateGuardianValidationSchema = z.object({
-  fatherName: z.string(),
-  fatherOccupation: z.string(),
-  fatherContactNo: z.string(),
-  motherName: z.string(),
-  motherOccupation: z.string(),
-  motherContactNo: z.string(),
+  fatherName: z.string().optional(),
+  fatherOccupation: z.string().optional(),
+  fatherContactNo: z.string().optional(),
+  motherName: z.string().optional(),
+  motherOccupation: z.string().optional(),
+  motherContactNo: z.string().optional(),
 });
 
 const updateLocalGuardianValidationSchema = z.object({
-  name: z.string(),
-  occupation: z.string(),
-  contactNo: z.string(),
-  address: z.string(),
+  name: z.string().optional(),
+  occupation: z.string().optional(),
+  contactNo: z.string().optional(),
+  address: z.string().optional(),
 });
 
 export const updateStudentValidationSchema = z.object({
   body: z.object({
-    password: z.string().max(20).optional(),
     student: z.object({
       name: updateUserNameValidationSchema,
-      gender: z.enum(['male', 'female', 'other']),
-      dateOfBirth: z
-        .string()
-        .refine((value) => !isNaN(Date.parse(value)), {
-          message: 'Invalid date format',
-        })
-        .transform((value) => new Date(value)),
-      email: z.string().email(),
-      contactNo: z.string(),
-      emergencyContactNo: z.string(),
-      bloogGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
-      presentAddress: z.string(),
-      permanentAddress: z.string(),
-      guardian: updateGuardianValidationSchema,
-      localGuardian: updateLocalGuardianValidationSchema,
-      admissionSemester: z.string(),
-      academicDepartment: z.string(),
+      gender: z.enum(['male', 'female', 'other']).optional(),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email().optional(),
+      contactNo: z.string().optional(),
+      emergencyContactNo: z.string().optional(),
+      bloogGroup: z
+        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+        .optional(),
+      presentAddress: z.string().optional(),
+      permanentAddress: z.string().optional(),
+      guardian: updateGuardianValidationSchema.optional(),
+      localGuardian: updateLocalGuardianValidationSchema.optional(),
+      admissionSemester: z.string().optional(),
+      profileImg: z.string().optional(),
+      academicDepartment: z.string().optional(),
     }),
   }),
 });
