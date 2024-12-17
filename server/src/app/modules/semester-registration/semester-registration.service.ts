12bb94224319f36e623ce6d9308249ce88b509d9
ashiqur-russel <ashiqurrussel2@gmail.com>
2024-12-17 01:48:03 +0100

    Create  basic CRUD boilerplate for semester registration with controller and service

---
+++ server/src/app/modules/semester-registration/semester-registration.service.ts
@@ -0,0 +1,26 @@
+import { TSemesterRegistration } from './semester-registration.interface';
+
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {};
+
+const getAllSemesterRegistrationsFromDB = async (
+  query: Record<string, unknown>,
+) => {};
+
+const getSingleSemesterRegistrationsFromDB = async (id: string) => {};
+
+const updateSemesterRegistrationIntoDB = async (
+  id: string,
+  payload: Partial<TSemesterRegistration>,
+) => {};
+
+const deleteSemesterRegistrationFromDB = async (id: string) => {};
+
+export const SemesterRegistrationService = {
+  createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationsFromDB,
+  getSingleSemesterRegistrationsFromDB,
+  updateSemesterRegistrationIntoDB,
+  deleteSemesterRegistrationFromDB,
+};
