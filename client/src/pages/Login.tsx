3fa695df6e079412578890b9474b6d37b9494091
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-01-12 18:50:32 +0100

    Implement base Api, fetch auth data using rtk query, setup basic login form and set auth state with user dsata and token

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,76 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: { id: "2025-01-0001", password: "12345" },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+
+    const result = await login(userInfo).unwrap();
+
+    const token = result.data.accessToken;
+
+    const user = verifyToken(token);
+
+    dispatch(setUser({ user: user, token: result.data.accessToken }));
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          flexDirection: "column",
+          width: "10%",
+        }}
+      >
+        <div>
+          <label htmlFor="id">ID:</label>
+          <input
+            type="text"
+            id="id"
+            {...register("id", { required: true })}
+            style={{ border: "1px solid gray" }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">PASSWORD:</label>
+          <input
+            type="password"
+            id="password"
+            {...register("password", { required: true })}
+            style={{ border: "1px solid gray" }}
+          />
+        </div>
+        <Button htmlType="submit">Login</Button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
-    
