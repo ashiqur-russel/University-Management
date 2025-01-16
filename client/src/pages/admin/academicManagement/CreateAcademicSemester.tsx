455d1cf24be2debd6090a44c72b06927b45ba9aa
ashiqur-russel <ashiqurrussel2@gmail.com>
2025-01-16 01:35:42 +0100

    Handle create semester form

--- client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -3,11 +3,38 @@ import AppForm from "../../../components/form/AppForm";
 import { Button, Col, Flex } from "antd";
 import FormSelect from "../../../components/form/FormSelect";
 
+const currentYear = new Date().getFullYear();
+
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
+
 const semesterOptions = [
   { value: "01", label: "Autumn" },
   { value: "02", label: "Summer" },
   { value: "03", label: "Fall" },
 ];
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const monthOptions = monthNames.map((item) => ({
+  value: item,
+  label: item,
+}));
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
@@ -16,6 +43,9 @@ const CreateAcademicSemester = () => {
     const semesterData = {
       name,
       code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
     };
 
     console.log(semesterData);
@@ -26,14 +56,18 @@ const CreateAcademicSemester = () => {
       <Col span={6}>
         <AppForm onSubmit={onSubmit} formName="creteSemesterForm">
           <FormSelect label="Name" name="name" options={semesterOptions} />
-          <FormSelect label="Year" name="name" options={semesterOptions} />
+          <FormSelect label="Year" name="year" options={yearOptions} />
 
           <FormSelect
             label="Start Month"
-            name="name"
-            options={semesterOptions}
+            name="startMonth"
+            options={monthOptions}
           />
-          <FormSelect label="End Month" name="name" options={semesterOptions} />
+          <FormSelect
+            label="End Month"
+            name="endMonth"
+            options={monthOptions}
+          />
 
           <Button htmlType="submit">Submit</Button>
         </AppForm>
